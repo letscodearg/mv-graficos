753a0fc41bf5636bb966c58c8cf4456d401de2c4
delficasarino <delficasarino@gmail.com>
2022-11-07 15:10:30 -0300

    updates equipos

--- src/pages/Equipos.js
+++ src/pages/Equipos.js
@@ -1,19 +1,20 @@
 import '../styles/Equipos.css';
+import equipo1 from '../assets/equipos/equipo1.jpeg';
 
 function Equipos() {
   return (
-    <main className="df wp rw jcc aic d-cl"> 
+    <main className="df wp rw jcc aic d-cl equipos"> 
       <section className="main d-df d-rw d-wp d-jcb d-ais">
         <h1>Equipamiento</h1>
-        <article>
-            <p>Contamos con diferentes maquinarias, para poder realizar el proceso completo: </p>
-            <ul>
-            <li>Cortadora de bobinas y guillotina</li>
-            <li>Impresoras offset planas de 6 colores más barniz en línea.</li>
-            <li>Troqueladoras con descartonado en línea</li>
-            <li>Pegadoras automáticas para estuches de fondo armado, pegado lateral y 4 o 6 ángulos.</li>
-            <li>Procesos Especiales: Horno para aplicación de Laca UV</li>
-            </ul>
+        <article className="df cl jcc aic">
+          <section className="df cl jcc aic imgBox">
+            <img src={equipo1}  alt="logo" />
+            <p>Cortadora de bobinas y guillotina</p>
+          </section>
+          <img src={equipo1}  alt="logo" />
+          <img src={equipo1}  alt="logo" />
+          <img src={equipo1}  alt="logo" />
+          <img src={equipo1}  alt="logo" />
         </article>
       </section>
       <section className="d-df d-wp d-rw d-jcc d-aic">
