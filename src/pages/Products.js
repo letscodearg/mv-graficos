967a4f42ff7c91e189a0012f5da67564174aaa8d
delficasarino <delficasarino@gmail.com>
2022-11-10 11:16:54 -0300

    update productos

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,4 +1,10 @@
 import '../styles/Products.css';
+import { Carousel } from "../components/Carousel";
+import celusal from "../assets/clients/celusal.png";
+import crysf from "../assets/clients/crysf.png";
+import maped from "../assets/clients/maped.png";
+import quaker from "../assets/clients/quaker.png";
+import burgerking from "../assets/clients/burgerking.png"
 
 function Products() {
   return (
@@ -11,14 +17,12 @@ function Products() {
             <p>También realizamos estuches de mayor tamaño realizados en microcorrugado.</p>
         </article>
       </section>
-      <section className="d-df d-wp d-rw d-jcc d-aic">
-        <article className="df wp rw jcc aic box">
-          
-        </article>
-        <article className="df wp rw jcc aic box">
-          
-        </article>
-      </section>
+      <Carousel
+        slides={[celusal, crysf, maped, quaker, burgerking]}
+        velocity={1000}
+        interval={5000}
+        height={"10rem"}
+      />
     </main>
   );
 }
