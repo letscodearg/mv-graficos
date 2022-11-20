5b830841aa8124e27b0e23be8e97222cc896e717
EduardoMVirgilio <eduardomiguevirgilio@gmail.com>
2022-11-20 16:06:52 -0300

    ✨ CallToAction

---
+++ src/components/CallToAction.js
@@ -0,0 +1,128 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+const CallToAction = ({ image, children, link }) => {
+  return (
+    <Container>
+      <Cover>
+        <img src={image} />
+      </Cover>
+      <Action>
+        {children}
+        <Link to={link}>ver más</Link>
+      </Action>
+    </Container>
+  );
+};
+
+const Container = styled.section`
+  position: relative;
+  width: 100vw;
+  height: 50vh;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  @media screen and (max-width: 1280px) {
+  }
+`;
+const Cover = styled.figure`
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  img {
+    max-width: 100%;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+const Action = styled.article`
+  z-index: 1;
+  align-self: center;
+  width: 48%;
+  display: flex;
+  flex-flow: column wrap;
+  align-items: flex-end;
+  justify-content: center;
+  background-color: rgba(240, 240, 240, 0);
+  padding: 1rem;
+  box-sizing: border-box;
+  position: relative;
+  overflow: hidden;
+  transition: all 0.3s;
+  cursor: pointer;
+  &::before {
+    content: "";
+    position: absolute;
+    width: 5%;
+    height: 100%;
+    background-color: rgba(228, 0, 42, 0);
+    transform: skewX(45deg) scale(2);
+    right: -10%;
+    transition: all 0.3s;
+  }
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+  h2,
+  h3 {
+    font-size: 2rem;
+    text-transform: uppercase;
+    font-family: "Josefin Slab", serif;
+    font-weight: 700;
+    opacity: 1;
+    transition: all 1s;
+    text-align: right;
+  }
+  p {
+    font-size: 1rem;
+    font-family: "Josefin Sans", sans-serif;
+    font-weight: 300;
+    opacity: 1;
+    transition: all 1s;
+  }
+  a {
+    text-decoration: none;
+    background-color: rgba(228, 0, 42, 1);
+    padding: 0.5rem 1.25rem;
+    color: rgb(240, 240, 240);
+    font-family: "Josefin Sans", sans-serif;
+    font-weight: 700;
+    margin: 1rem 0;
+    border-radius: 1rem;
+    text-transform: uppercase;
+  }
+  &:hover {
+    background-color: rgba(240, 240, 240, 1);
+    &::before {
+      right: 105%;
+      background-color: rgba(228, 0, 42, 1);
+    }
+    h2,
+    h3,
+    p {
+      opacity: 1;
+    }
+  }
+  @media screen and (max-width: 1280px) {
+    width: 100%;
+    display: flex;
+    align-self: flex-end;
+    background-color: rgba(240, 240, 240, 1);
+    &::before {
+      content: "";
+      position: absolute;
+      background-color: rgba(228, 0, 42, 1);
+      right: auto;
+      left: 0;
+      bottom: 0;
+      transition: all 0.3s;
+    }
+  }
+`;
+
+export default CallToAction;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,5 @@
 import "../styles/Home.css";
 import imgHome from "../assets/home/imgHome.jpg";
-import iso from '../assets/calidad/iso.jpg';
-import fsc from '../assets/calidad/fsc.png';
 import { Carousel } from "../components/Carousel";
 import trespatitos from "../assets/clients/trespatitos.png";
 import crysf from "../assets/clients/crysf.png";
@@ -10,27 +8,25 @@ import quaker from "../assets/clients/quaker.png";
 import burgerking from "../assets/clients/burgerking.png";
 import quickfood from "../assets/clients/quickfood.png";
 import celusal from "../assets/clients/celusal.png";
-import {Link} from "react-router-dom";
-
+import { Link } from "react-router-dom";
+import CallToAction from "../components/CallToAction";
 function Home() {
   return (
     <main className="df wp rw jcc aic home">
-      <section className="df rw wp jca aic d-jcb d-ais" id="banner">
-        <figure className="imgHome">
-          <img src={imgHome} alt="MV Gráficos" />
-        </figure>
-        <article className="text d-df d-rw d-wp d-jcb d-aic">
-          <section className="df cl d-df d-rw d-wp d-jcb d-aic bannerText">
-            <Link to="/nosotros">CALIDAD Y SERVICIO EN ESTUCHES DE CARTULINA Y MICROCORRUGADO</Link>
-{/*             <article className="df rw wp jce ais">
-                <img src={iso} alt="iso9001" id="iso"/>
-                <img src={fsc} alt="fsc" id="fsc"/>
-            </article> */}
-          </section>
-        </article>
-      </section>
+      <CallToAction image={imgHome} link="/nosotros">
+        <h2>La mejor calidad y servicio</h2>
+        <p>En estuches de cartulina y microcorrugado</p>
+      </CallToAction>
       <Carousel
-        slides={[trespatitos, crysf, starbucks, quaker, burgerking, quickfood, celusal]}
+        slides={[
+          trespatitos,
+          crysf,
+          starbucks,
+          quaker,
+          burgerking,
+          quickfood,
+          celusal,
+        ]}
         velocity={1000}
         interval={2000}
         height={"5rem"}
