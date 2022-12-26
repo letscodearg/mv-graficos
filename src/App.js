e067df5f439de119788c5ea2a3f0881445545be0
delficasarino <delficasarino@gmail.com>
2022-12-26 11:18:53 -0300

    update router

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ import Contacto from "./pages/Contacto";
 import Home from "./pages/Home";
 
 function App() {
-    return (
-        <Routes>
-            <Route path="/" element={<Layout />}>
-                <Route index exact="true" element={<Home />} />
-                <Route path="nosotros" exact="true" element={<About />} />
-                <Route path="lineas" exact="true" element={<Products />} />
-                <Route path="equipamiento" exact="true" element={<Equipos />} />
-                <Route path="calidad" exact="true" element={<Calidad />} />
-                <Route path="clientes" exact="true" element={<Clientes />} />
-                <Route path="contacto" exact="true" element={<Contacto />} />
-            </Route>
-        </Routes>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index exact="true" element={<Home />} />
+        <Route path="/nosotros" exact="true" element={<About />} />
+        <Route path="/lineas" exact="true" element={<Products />} />
+        <Route path="/equipamiento" exact="true" element={<Equipos />} />
+        <Route path="/calidad" exact="true" element={<Calidad />} />
+        <Route path="/clientes" exact="true" element={<Clientes />} />
+        <Route path="/contacto" exact="true" element={<Contacto />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
